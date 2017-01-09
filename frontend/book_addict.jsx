83afd7539dd543ce58573090f80e02fec537adb1
Your Name <you@example.com>
2017-01-09 11:59:40 -0800

    test root

--- frontend/book_addict.jsx
+++ frontend/book_addict.jsx
@@ -1,2 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>BookAddict</h1>, root);
+});
